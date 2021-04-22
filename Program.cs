853d08b3fb3ce92eb7970be4a09eb3385e69531e
Andres Sainz <andresmsainz@gmail.com>
2021-04-22 12:44:52 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataTypesInDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 0;
+            x = int.MaxValue;
+            Console.WriteLine("x = " + x);
+
+            int y = 2147483647;
+            Console.WriteLine("y = " + y);
+
+            float f_no = 0.0f;
+            double d_no = 0.0d;
+            decimal m_no = 0.0m;
+
+            f_no = 1 / 3f;
+            d_no = 1 / 3d;
+            m_no = 1 / 3m;
+
+            Console.WriteLine("f_no = " + f_no);
+            Console.WriteLine("d_no = " + d_no);
+            Console.WriteLine("m_no = " + m_no);
+
+
+        }
+    }
+}
